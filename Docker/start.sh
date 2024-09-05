0fbe112ccbe57cf6f9fbdf1647b909686e2c5820
nouman1007 <noumanshafiq84@gmail.com>
2024-09-05 22:43:38 +0500

    modify  docker files in the docker directory

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 /usr/sbin/sshd
-/usr/sbin/httpd -DFOREGROUND
+# /usr/sbin/httpd -DFOREGROUND
+
+# Start and enable SSH, run drush, and start Apache
+CMD ["sh", "-c", "service ssh start && sleep 10 && vendor/bin/drush updb -y && vendor/bin/drush cim -y && vendor/bin/drush cr && apache2ctl -D FOREGROUND"]
