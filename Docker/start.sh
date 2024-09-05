caf04a21adac5dc9ec58acf911bc63aa5e6816bf
nouman1007 <noumanshafiq84@gmail.com>
2024-09-05 23:25:47 +0500

    modified  Dockerfile in the docker directory

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-/usr/sbin/sshd
-# /usr/sbin/httpd -DFOREGROUND
 
-# Start and enable SSH, run drush, and start Apache
-CMD ["sh", "-c", "service ssh start && sleep 10 && vendor/bin/drush updb -y && vendor/bin/drush cim -y && vendor/bin/drush cr && apache2ctl -D FOREGROUND"]
+# Start the SSH daemon
+/usr/sbin/sshd
+
+# Start Apache server in the foreground
+apache2ctl -D FOREGROUND
